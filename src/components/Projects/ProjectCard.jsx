a93987352cd61b3d7870ea42541685978066ceb2
jay9949 <jay37192@gmail.com>
2023-12-12 14:44:57 +0530

    change done

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,33 +1,76 @@
 import React from "react";
 import netflix from "../../../assets/projects/netflix.png";
+import country from "../../../assets/projects/country.png";
+import tracker from "../../../assets/projects/IPTracker.png";
 import styles from "./ProjectCard.module.css";
-import { getImageUrl } from "../../utils";
 
-export const ProjectCard = ({
-  project: { title, description, skills, demo, source },
-}) => {
+export const ProjectCard = () => {
   return (
-    <div className={styles.container}>
-      <img src={netflix} alt={`Image of ${title}`} className={styles.image} />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
-        <a href={source} className={styles.link}>
-          Source
-        </a>
+    <>
+      <div className={styles.container}>
+        <img src={netflix} alt="netlix-clone" className={styles.image} />
+        <h3 className={styles.title}>Netflix-Clone</h3>
+        <p className={styles.description}>
+          This is a Netflix-clone and fetch by TMDB API Database from
+          backend/API.
+        </p>
+        <div className={styles.links}>
+          <a href="#" className={styles.link}>
+            Demo
+          </a>
+          <a href="#" className={styles.link}>
+            Source
+          </a>
+        </div>
       </div>
-    </div>
+      <div className={styles.container}>
+        <img src={country} alt="country" className={styles.image} />
+        <h3 className={styles.title}>Country App</h3>
+        <p className={styles.description}>
+          In this project I m using restcountries API & Filter country by
+          Search. Checkout this project
+        </p>
+        <div className={styles.links}>
+          <a href="#" className={styles.link}>
+            Demo
+          </a>
+          <a href="#" className={styles.link}>
+            Source
+          </a>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <img src={tracker} alt="ip-tracker" className={styles.image} />
+        <h3 className={styles.title}>IP Address Tracker</h3>
+        <p className={styles.description}>
+          In this project I m using API for tracking IP address & domain & also
+          given exact location.
+        </p>
+        <div className={styles.links}>
+          <a href="#" className={styles.link}>
+            Demo
+          </a>
+          <a href="#" className={styles.link}>
+            Source
+          </a>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <img src={tracker} alt="ip-tracker" className={styles.image} />
+        <h3 className={styles.title}>News Monkey</h3>
+        <p className={styles.description}>
+          In this project I m using news API & fetching data for daily updated
+          news & also using Scss
+        </p>
+        <div className={styles.links}>
+          <a href="#" className={styles.link}>
+            Demo
+          </a>
+          <a href="#" className={styles.link}>
+            Source
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import styles from "./Projects.module.css";
 
-import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
@@ -10,9 +9,7 @@ export const Projects = () => {
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+        <ProjectCard />
       </div>
     </section>
   );
